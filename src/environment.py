a4237640309e7bb1301610d9f9614300bd636351
vdblm <balazadehvahid@gmail.com>
2019-09-20 16:14:12 +0200

    chessboard env added

--- src/environment.py
+++ src/environment.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class EpisodicMDP:
-    def __init__(self, n_states, n_actions, ep_l):
+    def __init__(self, n_states, n_actions, ep_l, costs, transition_probs):
         """
         :param n_states: number of states
         :param n_actions: number of actions
@@ -16,14 +16,18 @@ class EpisodicMDP:
         :type n_states: int
         :type n_actions: int
         :type ep_l: int
+        :param costs: True costs
+        :type costs: dict
+        :param transition_probs: True transitions
+        :type transition_probs: dict
         """
         self.n_states = n_states
         self.n_actions = n_actions
         self.ep_l = ep_l
 
         # transition probs and costs
-        self.__costs = {}
-        self.__probs = {}
+        self.__costs = costs
+        self.__probs = transition_probs
 
         # current state and time
         self.state = 0
@@ -44,8 +48,8 @@ class EpisodicMDP:
         :return new_state, cost, finished
         :rtype (int, float, bool)
         """
-        cost = self.__costs[(self.state, action)]
-        self.state = np.random.choice(self.n_states, p=self.__probs[(self.state, action)])
+        cost = self.__costs[self.state, action]
+        self.state = np.random.choice(self.n_states, p=self.__probs[self.state, action])
 
         self.time_step += 1
         if self.time_step >= self.ep_l:
@@ -54,3 +58,64 @@ class EpisodicMDP:
             finished = False
 
         return self.state, cost, finished
+
+
+def make_chessboard(length=5, ep_l=15, eps=0.1, trans_prob=0.8):
+    """
+    Chessboard example in the paper
+    :param length: Number of cells = length * length
+    :type length: int
+    :param ep_l: episode length
+    :type ep_l: int
+    :param eps: the least cost
+    :type eps: float
+    :param trans_prob: The probability of a successful transition
+    :type trans_prob: float
+    """
+
+    # actions = {up, down, left, right}
+    n_actions = 4
+    n_states = length * length
+
+    # true costs and transitions
+    cost_true = {}
+    transition_true = {}
+
+    def next_state(x, y, a):
+        y_n = y + int(a < 2) * (-2 * a + 1)
+        x_n = x + int(a >= 2) * (2 * a - 5)
+        s_n = length * y_n + x_n
+        # if the next cell is wall
+        if s_n >= length * length or s_n < 0:
+            s_n = s
+        return s_n
+
+    # states = (xy) in base length. (00) is the start cell.
+    for x in range(length):
+        for y in range(length):
+            c = eps
+            if x > y and (x - y + 1) % 2 == 0:
+                c += (x - y + 1) / 2 * eps
+            elif y > x and (y - x) % 2 == 0:
+                c += (y - x) / 2 * eps
+            s = y * length + x
+            for a in range(n_actions):
+                cost_true[s, a] = c
+
+            # transitions
+            for a in range(n_actions):
+                transition_true[s, a] = np.zeros(n_states, dtype=np.float)
+                s_n = next_state(x, y, a)
+                transition_true[s, a][s_n] = trans_prob
+                for a2 in range(n_actions):
+                    if a2 != a:
+                        s_n = next_state(x, y, a2)
+                        transition_true[s, a][s_n] = (1 - trans_prob) / (n_actions - 1)
+                # normalize probs
+                transition_true[s, a] = np.divide(transition_true[s, a], sum(transition_true[s, a]))
+    # goal cost
+    for a in range(n_actions):
+        cost_true[length * 2 - 1, a] = 0
+
+    chessboard = EpisodicMDP(n_states, n_actions, ep_l, cost_true, transition_true)
+    return chessboard, cost_true, transition_true
